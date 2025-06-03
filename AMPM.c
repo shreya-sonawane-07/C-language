116f92180fbaf281056b2cee4585909f653367c3
shreya-sonawane-07 <shreyasonawane662@gmail.com>
2025-06-03 22:45:07 +0530

    Add files via upload

---
+++ AMPM.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h> 
+#define Size 50
+
+int main ()
+{
+    time_t t ;
+    struct tm *tmp ;
+    char MY_TIME[Size];
+    time( &t );
+    
+    
+    tmp = localtime( &t );
+    
+    // using strftime to display time
+    strftime(MY_TIME, sizeof(MY_TIME), "%x - %I:%M%p", tmp);
+    
+    printf("Formatted date & time : %s\n", MY_TIME );
+    return(0);
+}
